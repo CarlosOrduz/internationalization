bc0935750e11c46bb3f1490bcb4dbe6b9c937be7
CarlosOrduz <47404630+CarlosOrduz@users.noreply.github.com>
2020-04-20 16:43:34 -0500

    final

--- src/components/jobsList.js
+++ src/components/jobsList.js
@@ -1,53 +1,79 @@
-import React from 'react';
+import React from "react";
 import Job from "./job";
+import { FormattedMessage } from "react-intl";
 
 export default class JobsList extends React.Component {
-
-  state = { 
-  	"offers": [
-    	{
-    	  "id": "0001",
-        "name": "Manager",
-    	  "company": "Schneider Electric", 
-    	  "salary": 4.5,
-    	  "city": "Bogotá, Colombia",
-        "date": "2019-03-26"
-      }, 
+  state = {
+    offers: [
       {
-        "id": "0002",
-        "name": "Software Engineer",
-        "company": "Google Inc.", 
-        "salary": 20,
-        "city": "Palo Alto, CA, USA",
-        "date": "2019-03-27"
+        id: "0001",
+        name: "Manager",
+        company: "Schneider Electric",
+        salary: 4.5,
+        city: "Bogotá, Colombia",
+        date: "2019-03-26",
+        views: "134",
       },
       {
-        "id": "0003",
-        "name": "Nurse",
-        "company": "Clínica La Aurora", 
-        "salary": 1,
-        "city": "Cali, Colombia",
-        "date": "2019-03-28"
-      }
-    ]
+        id: "0002",
+        name: "Software Engineer",
+        company: "Google Inc.",
+        salary: 20,
+        city: "Palo Alto, CA, USA",
+        date: "2019-03-27",
+        views: "1250",
+      },
+      {
+        id: "0003",
+        name: "Nurse",
+        company: "Clínica La Aurora",
+        salary: 1,
+        city: "Cali, Colombia",
+        date: "2019-03-28",
+        views: "24000",
+      },
+    ],
   };
 
   render() {
+    function tableColor(){
+      var lang = navigator.language || navigator.userLanguage
+    if(lang.includes("es")){
+        return "thead-light";
+    }else if(lang.includes("en")){
+        return "thead-dark";
+    }
+      }
     return (
       <div>
         <table className="table">
-          <thead className="thead-dark">
+      <thead className={tableColor()}>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">Position</th>
-              <th scope="col">Company</th>
-              <th scope="col">Salary</th>
-              <th scope="col">City</th>
-              <th scope="col">Publication date</th>
+              <th scope="col">
+                <FormattedMessage id="Position" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Company" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Salary" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="City" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="PublicationDate" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Views" />
+              </th>
             </tr>
           </thead>
           <tbody>
-              {this.state.offers.map( (e,i) => <Job key={i} offer={e}/>)}
+            {this.state.offers.map((e, i) => (
+              <Job key={i} offer={e} />
+            ))}
           </tbody>
         </table>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+function lang(){
+    var lang = navigator.language || navigator.userLanguage
+    
+    if(lang.includes("es")){
+        return localeEsMessages;
+    }else if(lang.includes("en")){
+        return localeEnMessages;
+    }
+
+}
+function locale(){
+    
+
+    var lang = navigator.language || navigator.userLanguage;
+    
+    if(lang.includes("es")){
+        return "es";
+    }else if(lang.includes("en")){
+        return "en";
+    }
+
+}
+ReactDOM.render(
+	<IntlProvider locale={locale()} messages= {lang()}>
+		<JobsList/>
+       
+	</IntlProvider>, document.getElementById("root")
+);
